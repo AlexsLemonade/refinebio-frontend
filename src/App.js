bf5b4a1dc73a075bfc5468f05d7378ff61ce89aa
Stephanie Liu <stephofliu@gmail.com>
2018-03-22 14:55:10 -0400

    add Prettier as a pre-commit step

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import React, { Component } from "react";
-import logo from "./logo.svg";
-import styles from "./App.module.scss";
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import styles from './App.module.scss';
 
 class App extends Component {
   render() {
