e4335a1ffb3e95eb746f1e598db0b87e43a95c11
Ariel <arielsvn@gmail.com>
2018-05-17 17:21:50 -0400

    First part of experiments page

--- src/containers/Experiment/index.js
+++ src/containers/Experiment/index.js
@@ -2,15 +2,102 @@ import React from 'react';
 import { connect } from 'react-redux';
 import Loader from '../../components/Loader';
 import { fetchExperiment } from '../../state/experiment/actions';
+import Button from '../../components/Button';
+
+import './Experiment.scss';
+
+import AccessionIcon from '../../common/icons/accession.svg';
+import SampleIcon from '../../common/icons/sample.svg';
+import OrganismIcon from '../../common/icons/organism.svg';
 
 let Experiment = ({ fetch, experiment }) => (
-  <div>
-    <Loader fetch={fetch}>
-      {({ isLoading }) =>
-        isLoading ? <div className="loader" /> : <div>Experiment loaded</div>
-      }
-    </Loader>
-  </div>
+  <Loader fetch={fetch}>
+    {({ isLoading }) =>
+      isLoading ? (
+        <div className="loader" />
+      ) : (
+        <div>
+          <Button text="Back to Results" buttonStyle="secondary" />
+
+          <div className="experiment">
+            <div className="experiment__accession">
+              <img
+                src={AccessionIcon}
+                className="experiment__stats-icon"
+                alt="Accession Icon"
+              />
+              {experiment.accession_code}
+            </div>
+
+            <div className="experiment__header">
+              <h3>{experiment.title || 'No Title.'}</h3>
+              <Button text="Add to Dataset" />
+            </div>
+
+            <div className="experiment__stats">
+              <div>
+                <img
+                  src={AccessionIcon}
+                  className="experiment__stats-icon"
+                  alt="Accession Icon"
+                />{' '}
+                {experiment.id}
+              </div>
+              <div>
+                <img
+                  src={SampleIcon}
+                  className="experiment__stats-icon"
+                  alt="Sample Icon"
+                />{' '}
+                {experiment.samples.length} Samples
+              </div>
+              <div>
+                <img
+                  src={OrganismIcon}
+                  className="experiment__stats-icon"
+                  alt="Organism Icon"
+                />{' '}
+                {experiment.species}
+              </div>
+            </div>
+
+            <h4 className="experiment__title">
+              Submitter Supplied Information
+            </h4>
+
+            <div>
+              <div className="experiment__row">
+                <div className="experiment__row-label">Description</div>
+                <div>{experiment.description}</div>
+              </div>
+              <div className="experiment__row">
+                <div className="experiment__row-label">PubMed ID</div>
+                <div>{experiment.pubmed_id}</div>
+              </div>
+              <div className="experiment__row">
+                <div className="experiment__row-label">Publication Title</div>
+                <div>{experiment.publication_title}</div>
+              </div>
+              <div className="experiment__row">
+                <div className="experiment__row-label">
+                  Submitter’s Institution
+                </div>
+                <div>{experiment.submitter_institution}</div>
+              </div>
+              <div className="experiment__row">
+                <div className="experiment__row-label">Submitter</div>
+                <div>{experiment.pubmed_id}</div>
+              </div>
+            </div>
+
+            <hr className="experiment__divider" />
+
+            <h2>Samples</h2>
+          </div>
+        </div>
+      )
+    }
+  </Loader>
 );
 Experiment = connect(
   ({ experiment }) => ({ experiment }),
