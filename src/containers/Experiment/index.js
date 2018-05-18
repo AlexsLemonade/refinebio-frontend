cb9d0e1cbb8d8ffb357910ae4c38eb8987a85c6c
Ariel <arielsvn@gmail.com>
2018-05-18 11:50:14 -0400

    Add samples table and callbacks to add samples to dataset

--- src/containers/Experiment/index.js
+++ src/containers/Experiment/index.js
@@ -10,7 +10,15 @@ import AccessionIcon from '../../common/icons/accession.svg';
 import SampleIcon from '../../common/icons/sample.svg';
 import OrganismIcon from '../../common/icons/organism.svg';
 
-let Experiment = ({ fetch, experiment }) => (
+import ReactTable from 'react-table';
+import 'react-table/react-table.css';
+
+let Experiment = ({
+  fetch,
+  experiment,
+  addExperimentToDataset,
+  addSamplesToDataset
+}) => (
   <Loader fetch={fetch}>
     {({ isLoading }) =>
       isLoading ? (
@@ -31,7 +39,7 @@ let Experiment = ({ fetch, experiment }) => (
 
             <div className="experiment__header">
               <h3>{experiment.title || 'No Title.'}</h3>
-              <Button text="Add to Dataset" />
+              <Button text="Add to Dataset" onClick={addExperimentToDataset} />
             </div>
 
             <div className="experiment__stats">
@@ -92,7 +100,48 @@ let Experiment = ({ fetch, experiment }) => (
 
             <hr className="experiment__divider" />
 
-            <h2>Samples</h2>
+            <h2 className="experiment__title">Samples</h2>
+
+            <ReactTable
+              data={experiment.samples}
+              columns={[
+                {
+                  Header: 'Sample ID',
+                  accessor: 'id'
+                },
+                {
+                  Header: 'Title',
+                  accessor: 'title'
+                },
+                {
+                  Header: 'Age'
+                },
+                {
+                  Header: 'Gender'
+                },
+                {
+                  Header: 'Processing Information'
+                }
+              ]}
+            >
+              {(state, makeTable, instance) => {
+                return (
+                  <React.Fragment>
+                    <div className="experiment__sample-commands">
+                      <Button
+                        text="Add Page to Dataset"
+                        buttonStyle="secondary"
+                        onClick={() =>
+                          addSamplesToDataset(state.pageRows.map(x => x.id))
+                        }
+                      />
+                    </div>
+
+                    {makeTable()}
+                  </React.Fragment>
+                );
+              }}
+            </ReactTable>
           </div>
         </div>
       )
@@ -102,7 +151,15 @@ let Experiment = ({ fetch, experiment }) => (
 Experiment = connect(
   ({ experiment }) => ({ experiment }),
   (dispatch, ownProps) => ({
-    fetch: () => dispatch(fetchExperiment(ownProps.match.params.id))
+    fetch: () => dispatch(fetchExperiment(ownProps.match.params.id)),
+    addExperimentToDataset: () => {
+      let experimentId = ownProps.match.params.id;
+      // TODO: Add the current experiment to the Dataset
+      console.log('Add experiment to dataset: ', experimentId);
+    },
+    addSamplesToDataset: samples => {
+      console.log('TODO: add page to dataset', samples);
+    }
   })
 )(Experiment);
 
