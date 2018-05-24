d038caa6781a5736bd3e55521f3e1ce92387cc0f
Ariel <arielsvn@gmail.com>
2018-05-24 10:55:32 -0400

    Correct scrolltop possition on anchor

--- src/components/Anchor/index.js
+++ src/components/Anchor/index.js
@@ -1,6 +1,12 @@
 import React from 'react';
 
 /**
+ * This constant is used to correct the scroll bar position when navigating to an anchor
+ * Since we want the target element to appear below the scrollbar
+ */
+const TOP_BAR_HEIGHT = 110;
+
+/**
  * Creates an anchor class that scrolls to its possition if the correct hash has been specified
  * Only when the component is loaded
  */
@@ -11,7 +17,9 @@ export default class Anchor extends React.Component {
     const id = hash.replace('#', '');
     if (id === this.props.name) {
       setTimeout(() => {
-        this.element.scrollIntoView();
+        const offset = this.element.offsetTop;
+        // Substract the top bar height from the element's position
+        window.scrollTo({ top: offset - TOP_BAR_HEIGHT });
       }, 0);
     }
   }
