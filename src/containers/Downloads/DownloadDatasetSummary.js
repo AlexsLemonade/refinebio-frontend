3ecbdcd959a79b5f7f9189b5ff033707b7bc486b
Stephanie Liu <stephofliu@gmail.com>
2018-05-29 11:53:33 -0400

    extract into separate components

--- src/containers/Downloads/DownloadDatasetSummary.js
+++ src/containers/Downloads/DownloadDatasetSummary.js
@@ -1,42 +1,6 @@
 import React from 'react';
 
-const DownloadDatasetSummary = ({
-  samplesBySpecies,
-  totalSamples,
-  totalExperiments,
-  experimentCountBySpecies
-}) => {
-  const renderSpeciesRow = () => {
-    return Object.keys(samplesBySpecies).map((organism, i) => {
-      const samples = samplesBySpecies[organism];
-      return (
-        <tr className="downloads__table-row" key={i}>
-          <td className="downloads__table-cell">{organism}</td>
-          <td className="downloads__table-cell downloads__table-cell--value">
-            {samples.length}
-          </td>
-          <td className="downloads__table-cell downloads__table-cell--value">
-            {experimentCountBySpecies[organism]}
-          </td>
-        </tr>
-      );
-    });
-  };
-
-  const renderTotalsRow = () => {
-    return (
-      <tr className="downloads__table-row">
-        <td className="downloads__table-cell">Total</td>
-        <td className="downloads__table-cell downloads__table-cell--value">
-          {totalSamples}
-        </td>
-        <td className="downloads__table-cell downloads__table-cell--value">
-          {totalExperiments}
-        </td>
-      </tr>
-    );
-  };
-
+const DownloadDatasetSummary = props => {
   return (
     <section className="downloads__section">
       <h2>Dataset Summary</h2>
@@ -50,13 +14,44 @@ const DownloadDatasetSummary = ({
             </tr>
           </thead>
           <tbody>
-            {renderSpeciesRow()}
-            {renderTotalsRow()}
+            <SpeciesRow {...props} />
+            <TotalsRow {...props} />
           </tbody>
         </table>
       </div>
     </section>
   );
 };
+
+function SpeciesRow({ samplesBySpecies, experimentCountBySpecies }) {
+  return Object.keys(samplesBySpecies).map((organism, i) => {
+    const samples = samplesBySpecies[organism];
+    return (
+      <tr className="downloads__table-row" key={i}>
+        <td className="downloads__table-cell">{organism}</td>
+        <td className="downloads__table-cell downloads__table-cell--value">
+          {samples.length}
+        </td>
+        <td className="downloads__table-cell downloads__table-cell--value">
+          {experimentCountBySpecies[organism]}
+        </td>
+      </tr>
+    );
+  });
+}
+
+function TotalsRow({ totalSamples, totalExperiments }) {
+  return (
+    <tr className="downloads__table-row">
+      <td className="downloads__table-cell">Total</td>
+      <td className="downloads__table-cell downloads__table-cell--value">
+        {totalSamples}
+      </td>
+      <td className="downloads__table-cell downloads__table-cell--value">
+        {totalExperiments}
+      </td>
+    </tr>
+  );
+}
 
 export default DownloadDatasetSummary;
