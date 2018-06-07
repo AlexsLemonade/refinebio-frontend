b3806bceba85a65e865c54263fecf1fef5bdb09f
Ariel <arielsvn@gmail.com>
2018-06-07 15:24:03 -0400

    Add 404 page

---
+++ src/containers/NoMatch/index.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import RewardPoster from './404-reward-poster.svg';
+import Button from '../../components/Button';
+import './NoMatch.scss';
+
+import { goBack } from '../../state/routerActions';
+
+let NoMatch = ({ goBack }) => (
+  <div className="error-page">
+    <div className="error-page__text">
+      <h1>The page you are looking for isn’t expressed.</h1>
+      <Button text="Go Back" onClick={goBack} />
+    </div>
+
+    <div className="error-page__image">
+      <img src={RewardPoster} />
+    </div>
+  </div>
+);
+NoMatch = connect(() => ({}), { goBack })(NoMatch);
+
+export default NoMatch;
