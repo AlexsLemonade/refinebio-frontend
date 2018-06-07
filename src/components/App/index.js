d60f87b51b9a7d53295f80bcbbb16e9938725e64
Stephanie Liu <stephofliu@gmail.com>
2018-06-07 16:03:53 -0400

    adjust layout height

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -14,23 +14,21 @@ import DataSet from '../../containers/DataSet';
 
 const App = () => {
   return (
-    <div>
-      <Router history={history}>
-        <Layout>
-          <Switch>
-            <Route exact path="/" component={Main} />
-            <div className="layout__content">
-              <Route path="/results" component={Results} />
-              <Route path="/experiments/:id" component={Experiment} />
-              <Route path="/dashboard" component={Dashboard} />
-              <Route path="/download/:id" component={ViewDownload} />
-              <Route path="/download" component={Downloads} />
-              <Route path="/dataset/:id" component={DataSet} />
-            </div>
-          </Switch>
-        </Layout>
-      </Router>
-    </div>
+    <Router history={history}>
+      <Layout>
+        <Switch>
+          <Route exact path="/" component={Main} />
+          <div className="layout__content">
+            <Route path="/results" component={Results} />
+            <Route path="/experiments/:id" component={Experiment} />
+            <Route path="/dashboard" component={Dashboard} />
+            <Route path="/download/:id" component={ViewDownload} />
+            <Route path="/download" component={Downloads} />
+            <Route path="/dataset/:id" component={DataSet} />
+          </div>
+        </Switch>
+      </Layout>
+    </Router>
   );
 };
 
