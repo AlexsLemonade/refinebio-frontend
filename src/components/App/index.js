872e0cfc5d4c603013ff601fd8531a4fa6347e7c
Ariel <arielsvn@gmail.com>
2018-06-08 13:57:56 -0400

    Fix router after mergin

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -13,23 +13,34 @@ import Layout from '../Layout';
 import DataSet from '../../containers/DataSet';
 import NoMatch from '../../containers/NoMatch';
 
+/**
+ * The 404 route was giving conflicts when used inside App, that's it's extracted into
+ * this helper component.
+ */
+const AppContent = () => (
+  <div className="layout__content">
+    <Switch>
+      <Route path="/results" component={Results} />
+      <Route path="/experiments/:id" component={Experiment} />
+      <Route path="/dashboard" component={Dashboard} />
+      <Route path="/download/:id" component={ViewDownload} />
+      <Route path="/download" component={Downloads} />
+      <Route path="/dataset/:id" component={DataSet} />
+
+      {/* Custom route to be able to redirect to the 404 page */}
+      <Route path="/not-found" component={NoMatch} />
+      <Route path="*" component={NoMatch} />
+    </Switch>
+  </div>
+);
+
 const App = () => {
   return (
     <Router history={history}>
       <Layout>
         <Switch>
           <Route exact path="/" component={Main} />
-          <div className="layout__content">
-            <Route path="/results" component={Results} />
-            <Route path="/experiments/:id" component={Experiment} />
-            <Route path="/dashboard" component={Dashboard} />
-            <Route path="/download/:id" component={ViewDownload} />
-            <Route path="/download" component={Downloads} />
-            <Route path="/dataset/:id" component={DataSet} />
-            {/* Custom route to be able to redirect to the 404 page */}
-            <Route path="/not-found" component={NoMatch} />
-            <Route component={NoMatch} />
-          </div>
+          <Route path="/" component={AppContent} />
         </Switch>
       </Layout>
     </Router>
