4ab3a4a6b77186f2de65965a8a09b68b5ae3f706
Ariel <arielsvn@gmail.com>
2018-08-20 12:01:16 -0400

    Address comments in PR

--- src/api/dataSet.js
+++ src/api/dataSet.js
@@ -5,14 +5,12 @@ export async function getDataSet(dataSetId) {
 }
 
 export async function getDataSetDetails(dataSetId) {
-  return await Ajax.get(`/dataset/${dataSetId}/`, {details: true});
+  return await Ajax.get(`/dataset/${dataSetId}/`, { details: true });
 }
 
 export async function updateDataSet(dataSetId, dataSet) {
   return await Ajax.put(`/dataset/${dataSetId}/`, { data: dataSet });
 }
-
-
 
 // Takes in arrays of samples and experiments as formatted by the serializer
 // and turns them into objects where the keys are experiment accession codes
@@ -27,7 +25,7 @@ export function formatExperiments(experiments) {
 export function formatSamples(data, samples) {
   let result = {};
 
-  for (let accession_code in data) {
+  for (let accession_code of Object.keys(data)) {
     result[accession_code] = samples.filter(sample =>
       data[accession_code].includes(sample.accession_code)
     );
@@ -35,4 +33,3 @@ export function formatSamples(data, samples) {
 
   return result;
 }
-
