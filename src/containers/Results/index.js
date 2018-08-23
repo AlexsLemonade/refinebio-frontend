7306676fb88eb95784d81a0a3d0c880c6d14de88
Ariel <arielsvn@gmail.com>
2018-08-23 13:15:57 -0400

    fix lint issues

--- src/containers/Results/index.js
+++ src/containers/Results/index.js
@@ -86,10 +86,8 @@ class Results extends Component {
     const searchTerm = this.state.query;
     const {
       results,
-      toggledFilter,
       addExperiment,
       removeExperiment,
-      filters: filtersData,
       dataSet,
       isLoading,
       pagination: { totalPages, currentPage }
@@ -175,7 +173,6 @@ Results = connect(
     download: { dataSet }
   }) => ({
     results,
-    filters,
     pagination,
     searchTerm,
     dataSet,
