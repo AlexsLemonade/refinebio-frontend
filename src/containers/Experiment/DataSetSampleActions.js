896f1d5a3908ce3fd02f909cbc38ff031332c577
Ariel <arielsvn@gmail.com>
2018-08-28 14:36:15 -0400

    Add checkbox to show only processed samples

--- src/containers/Experiment/DataSetSampleActions.js
+++ src/containers/Experiment/DataSetSampleActions.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { connect } from 'react-redux';
 import Button from '../../components/Button';
 import { addExperiment, removeSamples } from '../../state/download/actions';
+import DataSetStats from './DataSetStats';
 
 /**
  * Given a dataset and a set of samples, this component renders the correct buttons
@@ -82,55 +83,6 @@ DataSetSampleActions = connect(
   }
 )(DataSetSampleActions);
 export default DataSetSampleActions;
-
-/**
- * Contains helper methods to calculate stats between a dataset and a list of samples
- * eg, how many processed samples are already in the dataset
- */
-export class DataSetStats {
-  constructor(dataSet, samples) {
-    this.dataSet = dataSet;
-    this.samples = samples;
-  }
-
-  anyProcessedSamples() {
-    return this.samples.some(x => x.is_processed);
-  }
-
-  anyUnprocessedSample() {
-    return this.samples.some(x => !x.is_processed);
-  }
-
-  allProcessedInDataSet() {
-    return this.getProcessedSamples().every(x => this.sampleInDataSet(x));
-  }
-
-  /**
-   * Returns true if a given sample is in the dataset
-   * @param {any} sample Sample object, with key `accession_code`
-   */
-  sampleInDataSet(sample) {
-    for (let experimentId in this.dataSet) {
-      // check if the passed sample is part of the experiment
-      if (
-        this.dataSet[experimentId].some(
-          sampleAccessionCode => sampleAccessionCode === sample.accession_code
-        )
-      ) {
-        return true;
-      }
-    }
-    return false;
-  }
-
-  getProcessedSamples() {
-    return this.samples.filter(x => x.is_processed);
-  }
-
-  getSamplesInDataSet() {
-    return this.samples.filter(x => x.is_processed && this.sampleInDataSet(x));
-  }
-}
 
 export function RemoveFromDatasetButton({
   handleRemove,
---
+++ src/containers/Experiment/DataSetStats.js
@@ -0,0 +1,48 @@
+/**
+ * Contains helper methods to calculate stats between a dataset and a list of samples
+ * eg, how many processed samples are already in the dataset
+ */
+export default class DataSetStats {
+  constructor(dataSet, samples) {
+    this.dataSet = dataSet;
+    this.samples = samples;
+  }
+
+  anyProcessedSamples() {
+    return this.samples.some(x => x.is_processed);
+  }
+
+  anyUnprocessedSample() {
+    return this.samples.some(x => !x.is_processed);
+  }
+
+  allProcessedInDataSet() {
+    return this.getProcessedSamples().every(x => this.sampleInDataSet(x));
+  }
+
+  /**
+   * Returns true if a given sample is in the dataset
+   * @param {any} sample Sample object, with key `accession_code`
+   */
+  sampleInDataSet(sample) {
+    for (let experimentId in this.dataSet) {
+      // check if the passed sample is part of the experiment
+      if (
+        this.dataSet[experimentId].some(
+          sampleAccessionCode => sampleAccessionCode === sample.accession_code
+        )
+      ) {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  getProcessedSamples() {
+    return this.samples.filter(x => x.is_processed);
+  }
+
+  getSamplesInDataSet() {
+    return this.samples.filter(x => x.is_processed && this.sampleInDataSet(x));
+  }
+}
