f42a5c3f0ec7cc921d7d5e08074e3db1c4ae85de
Ariel <arielsvn@gmail.com>
2018-09-17 12:14:40 -0400

    Refactor search page to use `Loader` component

--- src/containers/Results/index.js
+++ src/containers/Results/index.js
@@ -18,71 +18,39 @@ import GhostSampleImage from '../../common/images/ghost-sample.svg';
 import { Link } from 'react-router-dom';
 import DataSetSampleActions from '../Experiment/DataSetSampleActions';
 import isEqual from 'lodash/isEqual';
+import Loader from '../../components/Loader';
 
 class Results extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      isLoading: !props.results,
-      query: '',
-      filters: {}
-    };
-  }
-
-  componentDidMount() {
-    this.updateResults();
-  }
-
-  async componentDidUpdate(prevProps) {
-    if (prevProps.location.search !== this.props.location.search) {
-      // trigger a new search whenever the url changes, to ensure the results and the url
-      // are in sync
-      await this.updateResults();
-
-      // reset scroll position when the results change
-      window.scrollTo(0, 0);
-    }
-  }
+  state = {
+    query: '',
+    filters: {}
+  };
 
   /**
    * Reads the search query and other parameters from the url and submits a new request to update the results.
    */
   async updateResults() {
-    const { location } = this.props;
-    let { q: query, p: page, size, ...filters } = getQueryParamObject(
-      location.search
-    );
+    const searchArgs = this._parseUrl();
 
-    // for consistency, ensure all values in filters are arrays
-    // the method `getQueryParamObject` will return a single value for parameters that only
-    // appear once in the url
-    for (let key of Object.keys(filters)) {
-      if (!Array.isArray(filters[key])) {
-        filters[key] = [filters[key]];
-      }
-    }
+    this.setState({
+      query: searchArgs.query,
+      filters: searchArgs.filters
+    });
 
-    // parse parameters from url
-    query = decodeURIComponent(query);
-    page = parseInt(page || 1, 10);
-    size = parseInt(size || 10, 10);
-
-    this.setState({ query, filters });
-
-    // Check if we already have these results fetched, in which case we don't need to make an additional request
-    // this can only happen when the component is initially mounted.
+    // check if the search term and the filters are the same, in which case we don't need to
+    // fetch the results again
     if (
       this.props.results &&
       this.props.results.length > 0 &&
-      query === this.props.searchTerm &&
-      isEqual(filters, this.props.appliedFilters)
+      searchArgs.query === this.props.searchTerm &&
+      isEqual(searchArgs.filters, this.props.appliedFilters)
     ) {
       return;
     }
 
-    this.setState({ isLoading: true });
-    await this.props.fetchResults({ query, page, size, filters });
-    this.setState({ isLoading: false });
+    // reset scroll position when the results change
+    window.scrollTo(0, 0);
+    await this.props.fetchResults(searchArgs);
   }
 
   handleSubmit = values => {
@@ -102,19 +70,8 @@ class Results extends Component {
       results,
       addExperiment,
       removeExperiment,
-      isLoading,
       pagination: { totalPages, currentPage }
     } = this.props;
-
-    const samplesAsDataSet = results.reduce((data, result) => {
-      data[result.accession_code] = result.processed_samples.map(
-        accession_code => ({
-          accession_code,
-          is_processed: true
-        })
-      );
-      return data;
-    }, {});
 
     return (
       <div className="results">
@@ -127,50 +84,92 @@ class Results extends Component {
           <SearchInput onSubmit={this.handleSubmit} searchTerm={searchTerm} />
         </div>
 
-        {isLoading || this.state.isLoading ? (
-          <div className="loader" />
-        ) : !results.length ? (
-          <EmptyStates searchTerm={searchTerm} />
-        ) : (
-          <div className="results__container">
-            <div className="results__top-bar">
-              {results.length ? (
-                <div className="results__number-results">
-                  <NumberOfResults />
-                </div>
-              ) : null}
+        <Loader
+          updateProps={this.props.location.search}
+          fetch={() => this.updateResults()}
+        >
+          {({ isLoading }) =>
+            isLoading ? (
+              <div className="loader" />
+            ) : !results.length ? (
+              <EmptyStates
+                searchTerm={searchTerm}
+                appliedFilters={this.state.filters}
+              />
+            ) : (
+              <div className="results__container">
+                <div className="results__top-bar">
+                  <div className="results__number-results">
+                    <NumberOfResults />
+                  </div>
 
-              <DataSetSampleActions
-                data={samplesAsDataSet}
-                enableAddRemaining={false}
-                meta={{
-                  buttonStyle: 'secondary',
-                  addText: 'Add Page to Dataset'
-                }}
-              />
-            </div>
-            <div className="results__filters">
-              <ResultFilters appliedFilters={this.state.filters} />
-            </div>
-            <div className="results__list">
-              {results.map((result, i) => (
-                <Result
-                  key={i}
-                  result={result}
-                  addExperiment={addExperiment}
-                  removeExperiment={removeExperiment}
-                />
-              ))}
-              <Pagination
-                onPaginate={this.props.updatePage}
-                totalPages={totalPages}
-                currentPage={currentPage}
-              />
-            </div>
-          </div>
-        )}
+                  <DataSetSampleActions
+                    data={this._getSamplesAsDataSet()}
+                    enableAddRemaining={false}
+                    meta={{
+                      buttonStyle: 'secondary',
+                      addText: 'Add Page to Dataset'
+                    }}
+                  />
+                </div>
+                <div className="results__filters">
+                  <ResultFilters appliedFilters={this.state.filters} />
+                </div>
+                <div className="results__list">
+                  {results.map(result => (
+                    <Result
+                      key={result.accession_code}
+                      result={result}
+                      addExperiment={addExperiment}
+                      removeExperiment={removeExperiment}
+                    />
+                  ))}
+                  <Pagination
+                    onPaginate={this.props.updatePage}
+                    totalPages={totalPages}
+                    currentPage={currentPage}
+                  />
+                </div>
+              </div>
+            )
+          }
+        </Loader>
       </div>
     );
+  }
+
+  _parseUrl() {
+    let { q: query, p: page, size, ...filters } = getQueryParamObject(
+      this.props.location.search
+    );
+
+    // for consistency, ensure all values in filters are arrays
+    // the method `getQueryParamObject` will return a single value for parameters that only
+    // appear once in the url
+    for (let key of Object.keys(filters)) {
+      if (!Array.isArray(filters[key])) {
+        filters[key] = [filters[key]];
+      }
+    }
+
+    // parse parameters from url
+    query = decodeURIComponent(query);
+    page = parseInt(page || 1, 10);
+    size = parseInt(size || 10, 10);
+
+    return { query, page, size, filters };
+  }
+
+  _getSamplesAsDataSet() {
+    return this.props.results.reduce((data, result) => {
+      data[result.accession_code] = result.processed_samples.map(
+        accession_code => ({
+          accession_code,
+          is_processed: true
+        })
+      );
+      return data;
+    }, {});
   }
 }
 Results = connect(
