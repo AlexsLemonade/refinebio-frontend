d63719f066fcf89a38d84b771245767a92a742de
Kurt Wheeler <kurt.wheeler91@gmail.com>
2018-09-27 14:33:40 -0400

    Prevent deploys from non-tags by fixing circleci config.

---
+++ .circleci/deploy.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+# A tag is linked to a commit hash, not a branch. A single commit hash
+# can end up on multiple branches. So we first check to see if we're
+# on master, then on dev, then error out because we should only deploy master or dev.
+get_master_or_dev() {
+    master_check=$(git log --decorate=full | head -1 | grep origin/master || true)
+    dev_check=$(git log --decorate=full | head -1 | grep origin/dev || true)
+
+    if [[ ! -z $master_check ]]; then
+        echo "master"
+    elif [[ ! -z $dev_check ]]; then
+        echo "dev"
+    else
+        echo "Why in the world was update_docker_img.sh called from a branch other than dev or master?!?!?"
+        exit 1
+    fi
+}
+
+branch=$(get_master_or_dev)
+
+if [[ $branch == "master" ]]; then
+    base_host="refine.bio"
+elif [[ $branch == "dev" ]]; then
+    base_host="staging.refine.bio"
+else
+    echo "Why in the world was update_docker_img.sh called from a branch other than dev or master?!?!?"
+    exit 1
+fi
+
+yarn install --ignore-engines
+
+CI=false REACT_APP_API_HOST=https://api.$base_host yarn run build
+
+pip install awscli --upgrade --user
+
+~/.local/bin/aws s3 sync build s3://$base_host --delete --acl public-read
