0f6bffdf62df7b39164e8a2fdc24934b6ac4a1e7
Ariel <arielsvn@gmail.com>
2018-10-09 11:38:11 -0400

    additional tests to calculate the number of samples in the dataset

--- src/containers/Experiment/DataSetStats.test.js
+++ src/containers/Experiment/DataSetStats.test.js
@@ -1,14 +1,6 @@
 import DataSetStats from './DataSetStats';
 
 describe('DataSetStats', () => {
-  let samples = [
-    { accession_code: 's1', is_processed: true },
-    { accession_code: 's2', is_processed: true },
-    { accession_code: 's3', is_processed: true },
-    { accession_code: 's4', is_processed: false },
-    { accession_code: 's5', is_processed: false }
-  ];
-
   it('getAddedSlice', () => {
     let dataset = { e1: ['s1', 's2'] };
     const slice = { e1: ['s1'] };
@@ -16,14 +8,26 @@ describe('DataSetStats', () => {
     expect(addedSlice).toEqual({ e1: ['s1'] });
   });
 
-  it('returns samples in dataset', () => {
-    let dataset = { e1: ['s1', 's2'] };
-    const slice = { e1: ['s1'] };
-    let processedSamples = new DataSetStats(
-      dataset,
-      slice
-    ).getSamplesInDataSet();
-    expect(processedSamples).toEqual(['s1']);
+  describe('getSamplesInDataSet', () => {
+    it('returns samples in dataset', () => {
+      let dataset = { e1: ['s1', 's2'] };
+      const slice = { e1: ['s1'] };
+      let processedSamples = new DataSetStats(
+        dataset,
+        slice
+      ).getSamplesInDataSet();
+      expect(processedSamples).toEqual(['s1']);
+    });
+
+    it('returns unique values', () => {
+      let dataset = { e1: ['s1', 's2'], e2: ['s1'] };
+      const slice = { e1: ['s1'], e2: ['s1'] };
+      let processedSamples = new DataSetStats(
+        dataset,
+        slice
+      ).getSamplesInDataSet();
+      expect(processedSamples).toEqual(['s1']);
+    });
   });
 
   describe('all samples in dataset', () => {
