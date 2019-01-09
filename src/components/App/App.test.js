1d8f2aeb15a49631200d4b8f47ac17f7f20b6bb0
Ariel <arielsvn@gmail.com>
2019-01-09 16:17:38 -0500

    Check circleci with new end to end tests

--- src/components/App/App.test.js
+++ src/components/App/App.test.js
@@ -2,8 +2,51 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './index';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+import puppeteer from 'puppeteer';
+
+describe('refine.bio integration tests', () => {
+  test(
+    'landing page loads',
+    async () => {
+      const { page, close } = await loadPage('http://localhost:3000/');
+
+      await page.waitForSelector('.app-wrap');
+
+      close();
+    },
+    10000
+  );
+
+  test(
+    'search for samples',
+    async () => {
+      const { page, close } = await loadPage('http://localhost:3000/');
+
+      await page.waitForSelector('.app-wrap');
+
+      close();
+    },
+    10000
+  );
 });
+
+async function loadPage(url) {
+  let browser = await puppeteer.launch({
+    headless: true
+  });
+  let page = await browser.newPage();
+  page.emulate({
+    viewport: {
+      width: 1200,
+      height: 800
+    },
+    userAgent: ''
+  });
+
+  await page.goto(url);
+
+  return {
+    page,
+    close: () => browser.close()
+  };
+}
