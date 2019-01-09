a67b048c5e951123c70218d4b440dbfd244b5edd
Ariel <arielsvn@gmail.com>
2019-01-09 10:47:02 -0500

    update comment

--- src/api/dataSet.js
+++ src/api/dataSet.js
@@ -14,9 +14,13 @@ export async function getDataSetDetails(dataSetId) {
 }
 
 export async function updateDataSet(dataSetId, dataSet, details = false) {
-  // Q: is it wrong to include a query parameter in a PUT request?
-  // the `details` parameter is only used to "modify" the reponse from the API, it doesn't
-  // cause any side effect or different behavior
+  // It's not technically wrong to add a query parameter to the URL of a PUT request. ref https://github.com/AlexsLemonade/refinebio-frontend/pull/485#discussion_r246158557
+  // In this case, after a dataset is modified we'll need to fetch detailed information for it,
+  // which will include the samples grouped by organism for example.
+  // For a fully restfull api we could do a sepparate request for those, but that seems unnecessary
+  // since we can have that calculated on this request.
+  // When the api seels the `?details` parameter, it will include `experiments` and `organism_samples`
+  // in the response.
   const result = await Ajax.put(
     `/dataset/${dataSetId}/${details ? '?details=true' : ''}`,
     { data: dataSet }
@@ -32,7 +36,6 @@ export async function updateDataSet(dataSetId, dataSet, details = false) {
 
 // Takes in arrays of samples and experiments as formatted by the serializer
 // and turns them into objects where the keys are experiment accession codes
-
 export function formatExperiments(experiments) {
   return experiments.reduce((accum, experiment) => {
     accum[experiment.accession_code] = experiment;
