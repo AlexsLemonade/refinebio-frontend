23ffaf18faaa1ba4d108e569ba397d9803b2ebea
Ariel <arielsvn@gmail.com>
2019-01-28 16:48:06 -0500

    remove package

--- src/state/rootReducer.js
+++ src/state/rootReducer.js
@@ -3,12 +3,10 @@ import dashboard from './dashboard/reducer';
 import download from './download/reducer';
 import search from './search/reducer';
 import dataSet from './dataSet/reducer';
-import { reducer as formReducer } from 'redux-form';
 import token from './token';
 import experiment from './experiment/reducer';
 
 const rootReducer = combineReducers({
-  form: formReducer,
   dashboard,
   download,
   experiment,
