9b5a17f63d2a6964792fd74545d13cefddfe39b6
Ariel <arielsvn@gmail.com>
2019-02-08 15:42:37 -0500

    remove service worker

--- src/index.js
+++ src/index.js
@@ -2,11 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.scss';
 import App from './pages/App';
-
-import { unregister } from './registerServiceWorker';
-
 import smoothscroll from 'smoothscroll-polyfill';
-
 import 'delayed-scroll-restoration-polyfill';
 
 // kick off the polyfill!
@@ -19,14 +15,6 @@ function initApp() {
   document.querySelector('meta[name="description"]').remove();
 
   ReactDOM.render(<App />, document.getElementById('root'));
-
-  // Disable service worker https://github.com/facebook/create-react-app/issues/2715#issuecomment-313171863
-  // By default apps created with `create-react-app` (before v2.0.0) were offline first.
-  // This means that after the first visit, a cached version of the app is always served first
-  // In our case we want the latest version to always be served, and we don't need the app
-  // to work offline.
-  // More details at https://facebook.github.io/create-react-app/docs/making-a-progressive-web-app
-  unregister();
 }
 
 if (process.env.NODE_ENV === 'development') {
