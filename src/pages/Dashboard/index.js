79acffca10dd8389a64659f3c43f0cbc83148309
Ariel <arielsvn@gmail.com>
2019-03-06 15:01:09 -0500

    refactor dashboard

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -3,91 +3,61 @@ import Helmet from 'react-helmet';
 import * as chartSelectors from './chartSelectors';
 import DashboardSection from './DashboardSection';
 import TimeRangeSelect from '../../components/TimeRangeSelect';
-import Loader from '../../components/Loader';
+import Loader, { useLoader } from '../../components/Loader';
 import { timeout, getQueryParamObject } from '../../common/helpers';
+import { useInterval } from '../../common/hooks';
 import { fetchDashboardData } from '../../api/dashboad';
 import Spinner from '../../components/Spinner';
 
 import './Dashboard.scss';
 
-class Dashboard extends Component {
-  _liveUpdate = true;
+function Dashboard() {
+  const [range, setRange] = React.useState('year');
+  const { data, isLoading, refresh } = useLoader(
+    async () => {
+      const stats = await fetchDashboardData(range);
+      return getDashboardChartConfig(stats);
+    },
+    [range]
+  );
 
-  constructor(props) {
-    super(props);
+  // refresh data every 25 mins
+  useInterval(() => {
+    if (!!data) refresh();
+  }, 25 * 60 * 60 * 1000);
 
-    const { range } = getQueryParamObject(props.location.search);
-    this.state = {
-      timeRange: range || 'week',
-      firstRender: true
-    };
-  }
+  return (
+    <div className="dashboard">
+      <div className="dashboard__container">
+        <TimeRangeSelect
+          selectedOption={range}
+          options={[
+            { label: 'Today', value: 'day' },
+            { label: 'Last Week', value: 'week' },
+            { label: 'Last Month', value: 'month' },
+            { label: 'Last Year', value: 'year' }
+          ]}
+          onChange={setRange}
+        />
 
-  componentWillUnmount() {
-    // disable live updates after the component is unmounted
-    this._liveUpdate = false;
-  }
-
-  async updateData() {
-    const stats = await fetchDashboardData(this.state.timeRange);
-    this.setState({ firstRender: false });
-    this._startLiveUpdate();
-    return getDashboardChartConfig(stats);
-  }
-
-  async _startLiveUpdate() {
-    await timeout(5000);
-    if (this._liveUpdate) {
-      // update the data again, after a timeout
-      this.updateData();
-    }
-  }
-
-  render() {
-    return (
-      <div className="dashboard">
-        <Helmet>
-          <title>Dashboard - refine.bio</title>
-        </Helmet>
-        <div className="dashboard__container">
-          <TimeRangeSelect
-            initialValues={{ timeRange: this.state.timeRange }}
-            options={[
-              { label: 'Today', value: 'day' },
-              { label: 'Last Week', value: 'week' },
-              { label: 'Last Month', value: 'month' },
-              { label: 'Last Year', value: 'year' }
-            ]}
-            selectedTimeRange={range =>
-              console.log(range) || this.setState({ timeRange: range })
-            }
-          />
-
-          <Loader fetch={() => this.updateData()}>
-            {({ isLoading, data }) => {
-              if (isLoading) return <Spinner />;
-
-              return this.state.firstRender ? (
-                <div className="loader" />
-              ) : (
-                data.map((section, i) => {
-                  const { title, charts } = section;
-                  return (
-                    <DashboardSection
-                      key={i}
-                      title={title}
-                      charts={charts}
-                      isLoading={isLoading}
-                    />
-                  );
-                })
-              );
-            }}
-          </Loader>
-        </div>
+        {isLoading ? (
+          <Spinner />
+        ) : (
+          data.map(section => {
+            const { title, charts } = section;
+            return (
+              <DashboardSection
+                key={title}
+                title={title}
+                charts={charts}
+                isLoading={isLoading}
+              />
+            );
+          })
+        )}
       </div>
-    );
-  }
+    </div>
+  );
 }
 export default Dashboard;
 
@@ -110,33 +80,33 @@ function getDashboardChartConfig(state) {
     surveyJobsOverTimeByStatus
   } = {
     samplesCount: chartSelectors.getSamplesCount(state),
-    experimentsCount: chartSelectors.getExperimentsCount(state),
-    samplesAndExperimentsOverTime: chartSelectors.getSamplesAndExperimentsCreatedOverTime(
-      state
-    ),
-    jobsCompletedOverTime: chartSelectors.getJobsCompletedOverTime(state),
-    totalLengthOfQueuesByType: chartSelectors.getTotalLengthOfQueuesByType(
-      state
-    ),
-    averageTimesTilCompletion: chartSelectors.getAllAverageTimeTilCompletion(
-      state
-    ),
-    estimatedTimesTilCompletion: chartSelectors.getAllEstimatedTimeTilCompletion(
-      state
-    ),
-    jobsByStatus: chartSelectors.getJobsByStatus(state),
-    processorJobsOverTimeByStatus: chartSelectors.getJobsByStatusOverTime(
-      state,
-      'processor_jobs'
-    ),
-    surveyJobsOverTimeByStatus: chartSelectors.getJobsByStatusOverTime(
-      state,
-      'survey_jobs'
-    ),
-    downloaderJobsOverTimeByStatus: chartSelectors.getJobsByStatusOverTime(
-      state,
-      'downloader_jobs'
-    )
+    experimentsCount: chartSelectors.getExperimentsCount(state)
+    // samplesAndExperimentsOverTime: chartSelectors.getSamplesAndExperimentsCreatedOverTime(
+    //   state
+    // )
+    // jobsCompletedOverTime: chartSelectors.getJobsCompletedOverTime(state),
+    // totalLengthOfQueuesByType: chartSelectors.getTotalLengthOfQueuesByType(
+    //   state
+    // ),
+    // averageTimesTilCompletion: chartSelectors.getAllAverageTimeTilCompletion(
+    //   state
+    // ),
+    // estimatedTimesTilCompletion: chartSelectors.getAllEstimatedTimeTilCompletion(
+    //   state
+    // ),
+    // jobsByStatus: chartSelectors.getJobsByStatus(state),
+    // processorJobsOverTimeByStatus: chartSelectors.getJobsByStatusOverTime(
+    //   state,
+    //   'processor_jobs'
+    // ),
+    // surveyJobsOverTimeByStatus: chartSelectors.getJobsByStatusOverTime(
+    //   state,
+    //   'survey_jobs'
+    // ),
+    // downloaderJobsOverTimeByStatus: chartSelectors.getJobsByStatusOverTime(
+    //   state,
+    //   'downloader_jobs'
+    // )
   };
 
   return [
@@ -154,108 +124,107 @@ function getDashboardChartConfig(state) {
           data: samplesCount,
           type: 'text',
           size: 'small'
-        },
-        {
-          title: 'Samples and Experiments Created Over Time',
-          data: samplesAndExperimentsOverTime,
-          series: ['experiments', 'samples'],
-          type: 'line',
-          size: 'large'
         }
+        // {
+        //   title: 'Samples and Experiments Created Over Time',
+        //   data: samplesAndExperimentsOverTime,
+        //   series: ['experiments', 'samples'],
+        //   type: 'line',
+        //   size: 'large'
+        // }
       ]
     },
     {
       title: 'Jobs',
       charts: [
-        {
-          title: 'Jobs Over Time by Type',
-          data: jobsCompletedOverTime,
-          type: 'line',
-          series: ['survey', 'processor', 'downloader'],
-          size: 'large'
-        },
-        {
-          title: 'Total Length of Queues by Type',
-          data: totalLengthOfQueuesByType,
-          type: 'pie',
-          size: 'medium'
-        },
-
-        {
-          title: 'Survey Jobs by Status',
-          data: jobsByStatus.survey_jobs,
-          type: 'pie',
-          size: 'medium'
-        },
-        {
-          title: 'Processor Jobs by Status',
-          data: jobsByStatus.processor_jobs,
-          type: 'pie',
-          size: 'medium'
-        },
-        {
-          title: 'Downloader Jobs by Status',
-          data: jobsByStatus.downloader_jobs,
-          type: 'pie',
-          size: 'medium'
-        },
-        {
-          title: 'Average Time: Survey Jobs',
-          data: averageTimesTilCompletion.survey_jobs,
-          type: 'text',
-          size: 'small'
-        },
-        {
-          title: 'Estimated Time Till Completion: Survey Jobs',
-          data: estimatedTimesTilCompletion.survey_jobs,
-          type: 'text',
-          size: 'small'
-        },
-        {
-          title: 'Average Time: Processor Jobs',
-          data: averageTimesTilCompletion.processor_jobs,
-          type: 'text',
-          size: 'small'
-        },
-        {
-          title: 'Estimated Time Till Completion: Processor Jobs',
-          data: estimatedTimesTilCompletion.processor_jobs,
-          type: 'text',
-          size: 'small'
-        },
-        {
-          title: 'Average Time: Downloader Jobs',
-          data: averageTimesTilCompletion.downloader_jobs,
-          type: 'text',
-          size: 'small'
-        },
-        {
-          title: 'Estimated Time Till Completion: Downloader Jobs',
-          data: estimatedTimesTilCompletion.downloader_jobs,
-          type: 'text',
-          size: 'small'
-        },
-        {
-          title: 'Processor Jobs Over Time by Status',
-          data: processorJobsOverTimeByStatus,
-          type: 'line',
-          series: ['pending', 'open', 'completed', 'failed'],
-          size: 'large'
-        },
-        {
-          title: 'Survey Jobs Over Time by Status',
-          data: surveyJobsOverTimeByStatus,
-          type: 'line',
-          series: ['pending', 'open', 'completed', 'failed'],
-          size: 'large'
-        },
-        {
-          title: 'Downloader Jobs Over Time by Status',
-          data: downloaderJobsOverTimeByStatus,
-          type: 'line',
-          series: ['pending', 'open', 'completed', 'failed'],
-          size: 'large'
-        }
+        // {
+        //   title: 'Jobs Over Time by Type',
+        //   data: jobsCompletedOverTime,
+        //   type: 'line',
+        //   series: ['survey', 'processor', 'downloader'],
+        //   size: 'large'
+        // },
+        // {
+        //   title: 'Total Length of Queues by Type',
+        //   data: totalLengthOfQueuesByType,
+        //   type: 'pie',
+        //   size: 'medium'
+        // },
+        // {
+        //   title: 'Survey Jobs by Status',
+        //   data: jobsByStatus.survey_jobs,
+        //   type: 'pie',
+        //   size: 'medium'
+        // },
+        // {
+        //   title: 'Processor Jobs by Status',
+        //   data: jobsByStatus.processor_jobs,
+        //   type: 'pie',
+        //   size: 'medium'
+        // },
+        // {
+        //   title: 'Downloader Jobs by Status',
+        //   data: jobsByStatus.downloader_jobs,
+        //   type: 'pie',
+        //   size: 'medium'
+        // },
+        // {
+        //   title: 'Average Time: Survey Jobs',
+        //   data: averageTimesTilCompletion.survey_jobs,
+        //   type: 'text',
+        //   size: 'small'
+        // },
+        // {
+        //   title: 'Estimated Time Till Completion: Survey Jobs',
+        //   data: estimatedTimesTilCompletion.survey_jobs,
+        //   type: 'text',
+        //   size: 'small'
+        // },
+        // {
+        //   title: 'Average Time: Processor Jobs',
+        //   data: averageTimesTilCompletion.processor_jobs,
+        //   type: 'text',
+        //   size: 'small'
+        // },
+        // {
+        //   title: 'Estimated Time Till Completion: Processor Jobs',
+        //   data: estimatedTimesTilCompletion.processor_jobs,
+        //   type: 'text',
+        //   size: 'small'
+        // },
+        // {
+        //   title: 'Average Time: Downloader Jobs',
+        //   data: averageTimesTilCompletion.downloader_jobs,
+        //   type: 'text',
+        //   size: 'small'
+        // },
+        // {
+        //   title: 'Estimated Time Till Completion: Downloader Jobs',
+        //   data: estimatedTimesTilCompletion.downloader_jobs,
+        //   type: 'text',
+        //   size: 'small'
+        // },
+        // {
+        //   title: 'Processor Jobs Over Time by Status',
+        //   data: processorJobsOverTimeByStatus,
+        //   type: 'line',
+        //   series: ['pending', 'open', 'completed', 'failed'],
+        //   size: 'large'
+        // },
+        // {
+        //   title: 'Survey Jobs Over Time by Status',
+        //   data: surveyJobsOverTimeByStatus,
+        //   type: 'line',
+        //   series: ['pending', 'open', 'completed', 'failed'],
+        //   size: 'large'
+        // },
+        // {
+        //   title: 'Downloader Jobs Over Time by Status',
+        //   data: downloaderJobsOverTimeByStatus,
+        //   type: 'line',
+        //   series: ['pending', 'open', 'completed', 'failed'],
+        //   size: 'large'
+        // }
       ]
     }
   ];
