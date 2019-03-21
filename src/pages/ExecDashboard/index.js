bcf651454885aa9499789611709d9f14431527bf
Ariel <arielsvn@gmail.com>
2019-03-21 11:19:17 -0400

    typo

--- src/pages/ExecDashboard/index.js
+++ src/pages/ExecDashboard/index.js
@@ -29,7 +29,7 @@ export default function ExecutiveDashboard() {
 
       {!data ? (
         <Spinner />
-      ) : hasError || true ? (
+      ) : hasError ? (
         <ServerErrorPage />
       ) : (
         <>
