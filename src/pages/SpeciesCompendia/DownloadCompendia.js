ee0c3559aa553cf124c3f8aee5e1239888bce461
Ariel <arielsvn@gmail.com>
2019-05-01 14:37:49 -0400

    add tos checkbox

--- src/pages/SpeciesCompendia/DownloadCompendia.js
+++ src/pages/SpeciesCompendia/DownloadCompendia.js
@@ -1,11 +1,17 @@
 import React from 'react';
+import { connect } from 'react-redux';
 import Dropdown from '../../components/Dropdown';
+import Button from '../../components/Button';
 import { Ajax, formatSentenceCase } from '../../common/helpers';
 import { useLoader } from '../../components/Loader';
+import { Link } from 'react-router-dom';
+import Checkbox from '../../components/Checkbox';
 
 import groupBy from 'lodash/groupBy';
 
-const DownloadCompendia = props => {
+let DownloadCompendia = ({ agreedToTerms }) => {
+  const [selected, setSelected] = React.useState(null);
+  const [agree, setAgree] = React.useState(agreedToTerms);
   const { data, isLoading } = useLoader(fetchCompendiaData);
 
   if (isLoading || data.length === 0) return null;
@@ -18,26 +24,46 @@ const DownloadCompendia = props => {
         <div>
           <span className="download-compendia__choose">Choose Organism</span>
           <Dropdown
-            options={data}
-            selectedOption={props.selected}
-            label={x => formatSentenceCase(x.organism)}
-            onChange={selected => {}}
+            options={data.map(x => x.organism)}
+            selectedOption={selected}
+            label={formatSentenceCase}
+            onChange={setSelected}
           />
         </div>
 
-        <a
-          href={props.selected ? props.selected.url : data[0].url}
-          className="button download-compendia__button"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Download Now
-        </a>
+        <div className="download-compendia__row">
+          {!agreedToTerms && (
+            <div>
+              <Checkbox
+                name="termsOfService"
+                checked={agree}
+                onClick={() => setAgree(!agree)}
+              >
+                I agree to the{' '}
+                <Link
+                  to="/terms"
+                  className="link"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  Terms of Use
+                </Link>
+              </Checkbox>
+            </div>
+          )}
+          <Button
+            text="Download Now"
+            className="download-compendia__button"
+            isDisabled={!agree}
+          />
+        </div>
       </div>
     </div>
   );
 };
-
+DownloadCompendia = connect(state => ({
+  agreedToTerms: !!state.token
+}))(DownloadCompendia);
 export default DownloadCompendia;
 
 async function fetchCompendiaData() {
