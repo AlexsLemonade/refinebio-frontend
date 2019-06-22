3d9cfafb1c87ea29620c975565010210bf3d887b
David Mejia <davidsmejia@gmail.com>
2019-06-22 18:11:37 -0400

    adds loading state for onClicks that return a promise

--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -6,20 +6,49 @@ const Button = ({
   text,
   buttonStyle,
   isDisabled = false,
-  className,
+  className = '',
   children,
   type = 'button',
+  onClick,
   ...props
 }) => {
+  const loadingRef = React.useRef(false);
+  const promiseRef = React.useRef();
+
+  const buttonClick = () => {
+    if (typeof onClick === 'function') {
+      if (loadingRef.current) return promiseRef.curent;
+      const maybePromise = onClick();
+      if (maybePromise && typeof maybePromise.then === 'function') {
+        promiseRef.current = maybePromise;
+        loadingRef.current = true;
+        maybePromise.then(() => {
+          promiseRef.current = undefined;
+          loadingRef.current = false;
+        });
+      }
+      return maybePromise;
+    }
+    return onClick;
+  };
+
+  const { current: isLoading } = loadingRef;
   const buttonStyleClass = buttonStyle ? `button--${buttonStyle}` : '';
   /* eslint-disable react/button-has-type */
   return (
     <button
       {...props}
+      onClick={buttonClick}
       type={type}
-      className={classnames('button', buttonStyleClass, className)}
+      className={classnames({
+        button: true,
+        [buttonStyleClass]: true,
+        [className]: true,
+        'button--loading': isLoading,
+      })}
       disabled={isDisabled}
     >
+      {isLoading && <span className="button--indicator" />}
       {text || children}
     </button>
   );
