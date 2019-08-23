8f8aa4f8013c92bc4f075ec5cf21c34a245f226a
David Mejia <davidsmejia@gmail.com>
2019-08-23 10:52:20 -0400

    remove sitemap code and add api version

--- cacheBackend.js
+++ cacheBackend.js
@@ -6,16 +6,15 @@
 
 const axios = require('axios');
 const fs = require('fs');
-const sm = require('sitemap');
 const _ = require('lodash');
 const slugify = require('./src/common/slugify');
 
 const version = process.env.VERSION || '0.0.0';
-const ApiHost = process.env.REACT_APP_API_HOST || 'https://api.refine.bio';
+const ApiVersion = 'v1';
+const ApiHost = `${process.env.REACT_APP_API_HOST || 'https://api.refine.bio'}/${ApiVersion}`;
 
 // Call scripts at deploy time
 cacheServerData();
-generateSitemap();
 
 async function cacheServerData() {
   console.log(`Fetching data to be cached from endpoint: ${ApiHost}`);
@@ -39,54 +38,6 @@ async function cacheServerData() {
     platforms,
   };
   fs.writeFileSync(`src/apiData.json`, JSON.stringify(cache));
-}
-
-/**
- * Generates a sitemap by querying the API to get all experiments accession codes
- */
-async function generateSitemap() {
-  console.log('Building sitemap...');
-  // number of experiments retrieved on each request
-  const limit = 500;
-  const sitemap = sm.createSitemap({
-    hostname: 'https://www.refine.bio',
-    cacheTime: 600000,
-    urls: [
-      { url: '/about', priority: 0.5 },
-      { url: '/license', priority: 0.5 },
-      { url: '/privacy', priority: 0.5 },
-      { url: '/terms', priority: 0.5 },
-    ],
-  });
-
-  // get the total number of experiments
-  const {
-    data: { count },
-  } = await axios.get(`${ApiHost}/search/?limit=${0}`);
-
-  const pageOffsets = _.range(0, count, limit);
-  const experimentPages = await Promise.all(
-    pageOffsets.map(offset =>
-      axios
-        .get(`${ApiHost}/search/?limit=${limit}&offset=${offset}`)
-        .then(request => request.data.results)
-        .catch(ignore => [])
-    )
-  );
-
-  for (const results of experimentPages) {
-    for (const experiment of results) {
-      sitemap.add({
-        url: `/experiments/${experiment.accession_code}/${slugify(
-          experiment.title
-        )}`,
-        priority: 0.8,
-      });
-    }
-  }
-
-  console.log(`Sitemap generated. ${count} experiments found.`);
-  fs.writeFileSync('./public/sitemap.xml', sitemap.toString());
 }
 
 /**
