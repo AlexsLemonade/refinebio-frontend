cac91a9ad74a3178bb32c448bfd7557c9d5aef1d
David Mejia <davidsmejia@gmail.com>
2019-08-26 15:36:03 -0400

    mc -> hs

--- src/pages/Main/EmailSection.js
+++ src/pages/Main/EmailSection.js
@@ -7,43 +7,33 @@ export default function EmailSection() {
       <div className="main__blurp-text">
         Be the first to know about new features, compendia releases, and more!
       </div>
-      {/* Mailchimp Form Embed */}
-      <div id="mc_embed_signup">
+      {/* Email Subscribe Form */}
+      <div id="email-form">
         <form
-          action="https://ccdatalab.us14.list-manage.com/subscribe/post?u=1ed0ac5b8f11380ee862cf278&amp;id=074bca87ce"
-          method="post"
-          id="mc-embedded-subscribe-form"
-          name="mc-embedded-subscribe-form"
+          action="https://share.hsforms.com/1ZSsctZ2VT-y_dwRbTZ13Aw336z0"
+          method="get"
+          id="embedded-subscribe-form"
+          name="embedded-subscribe-form"
           className="validate"
           target="_blank"
           noValidate
         >
-          <div id="mc_embed_signup_scroll" className="main__mailchimp-form">
+          <div id="hs_embed_signup_scroll" className="main__email-section-form">
             <input
               type="email"
-              name="EMAIL"
-              className="input-text main__mailchimp-input"
-              id="mce-EMAIL"
+              name="email"
+              className="input-text main__email-section-input"
+              id="email"
               placeholder="email address"
               required
             />
-            <div
-              style={{ position: 'absolute', left: '-5000px' }}
-              aria-hidden="true"
-            >
-              <input
-                type="text"
-                name="b_1ed0ac5b8f11380ee862cf278_074bca87ce"
-                tabIndex="-1"
-              />
-            </div>
             <div className="flex-button-container">
               <input
                 type="submit"
                 value="Subscribe"
                 name="subscribe"
-                id="mc-embedded-subscribe"
-                className="button main__mailchimp-button"
+                id="hs-embedded-subscribe"
+                className="button main__email-section-button"
               />
             </div>
           </div>
