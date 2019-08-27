8232b1a40961a72dcf39f689aa1324120663819a
David Mejia <davidsmejia@gmail.com>
2019-08-27 12:09:35 -0400

    ajax email submission to hubspot

--- src/pages/Main/EmailSection.js
+++ src/pages/Main/EmailSection.js
@@ -1,11 +1,47 @@
 import React from 'react';
+import { Ajax } from '../../common/helpers';
 
 export default function EmailSection() {
+  const [email, setEmail] = React.useState(null);
+  const [subscribed, setSubscribed] = React.useState(null);
+
+  const portalId = '5187852';
+  const formGuid = '2a5f706e-eca8-45c0-a9f4-b1584c8c160a';
+  const formUrl = `https://api.hsforms.com/submissions/v3/integration/submit/${portalId}/${formGuid}`;
+  const getFormData = () => ({
+    fields: [
+      {
+        name: 'email',
+        value: email,
+      },
+    ],
+    // "context": {
+    // "hutk": ":hutk",
+    // "pageUri": window.location.host + window.location.pathname,
+    // "pageName": document.title,
+  });
+
+  const handleSubmit = async e => {
+    e.preventDefault();
+    try {
+      const result = await Ajax.post(formUrl, getFormData());
+      setSubscribed(result.inlineMessage);
+    } catch (err) {
+      // Show error message setError(true); "Please submit a valid e-mail address."
+      // console.log(err);
+    }
+  };
+
   return (
     <div className="main__container">
-      <div className="main__heading-1">Sign Up for Updates</div>
+      <div className="main__heading-1">
+        {!subscribed && 'Sign Up for Updates'}
+        {subscribed && 'Thanks!'}
+      </div>
       <div className="main__blurp-text">
-        Be the first to know about new features, compendia releases, and more!
+        {!subscribed &&
+          'Be the first to know about new features, compendia releases, and more!'}
+        {subscribed}
       </div>
       {/* Email Subscribe Form */}
       <div id="email-form">
@@ -16,6 +52,7 @@ export default function EmailSection() {
           name="embedded-subscribe-form"
           className="validate"
           target="_blank"
+          onSubmit={handleSubmit}
           noValidate
         >
           <div id="hs_embed_signup_scroll" className="main__email-section-form">
@@ -25,6 +62,7 @@ export default function EmailSection() {
               className="input-text main__email-section-input"
               id="email"
               placeholder="email address"
+              onChange={e => setEmail(e.target.value)}
               required
             />
             <div className="flex-button-container">
