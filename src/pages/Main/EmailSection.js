5ec64adf9efc04a7908f0efa8b98f49ee8762cc1
David Mejia <davidsmejia@gmail.com>
2019-08-31 20:08:26 -0400

    adds in email error support

--- src/pages/Main/EmailSection.js
+++ src/pages/Main/EmailSection.js
@@ -1,18 +1,21 @@
 import React from 'react';
+import classnames from 'classnames';
+import * as Yup from 'yup';
+import { Formik, Field, Form } from 'formik';
 import { Ajax } from '../../common/helpers';
+import Button from '../../components/Button';
+import Error from '../../components/Error';
 
 export default function EmailSection() {
   const [email, setEmail] = React.useState(null);
-  const [subscribed, setSubscribed] = React.useState(null);
-
   const portalId = '5187852';
   const formGuid = '2a5f706e-eca8-45c0-a9f4-b1584c8c160a';
   const formUrl = `https://api.hsforms.com/submissions/v3/integration/submit/${portalId}/${formGuid}`;
-  const getFormData = () => ({
+  const getFormData = submitEmail => ({
     fields: [
       {
         name: 'email',
-        value: email,
+        value: submitEmail,
       },
     ],
     // "context": {
@@ -21,25 +24,24 @@ export default function EmailSection() {
     // "pageName": document.title,
   });
 
-  const handleSubmit = async e => {
-    e.preventDefault();
+  const handleSubmit = async (values, { setSubmitting }) => {
+    setSubmitting(true);
     try {
-      await Ajax.post(formUrl, getFormData());
-      setSubscribed(true);
-    } catch (err) {
-      // Show error message setError(true); "Please submit a valid e-mail address."
-      // console.log(err);
+      await Ajax.post(formUrl, getFormData(values.email));
+      setEmail(values.email);
+    } catch (e) {
+      setEmail(null);
     }
+    setSubmitting(false);
   };
 
   return (
     <div className="main__container">
       <div className="main__heading-1">
-        {!subscribed && 'Sign Up for Updates'}
-        {subscribed && 'Thanks!'}
+        {email ? 'Thanks' : 'Sign Up for Updates'}
       </div>
       <div className="main__blurp-text">
-        {subscribed ? (
+        {email ? (
           <span>
             {' '}
             Thank you for subscribing. Updates will be sent to{' '}
@@ -51,40 +53,47 @@ export default function EmailSection() {
       </div>
       {/* Email Subscribe Form */}
       <div id="email-form">
-        <form
-          action="https://share.hsforms.com/1ZSsctZ2VT-y_dwRbTZ13Aw336z0"
-          method="get"
-          id="embedded-subscribe-form"
-          name="embedded-subscribe-form"
-          className="validate"
-          target="_blank"
+        <Formik
           onSubmit={handleSubmit}
-          noValidate
-          style={{
-            visibility: subscribed ? 'hidden' : 'visible',
-          }}
+          initialValues={{ email: '' }}
+          validationSchema={Yup.object().shape({
+            email: Yup.string()
+              .email('Please enter a valid email')
+              .required('Please enter your email adress'),
+          })}
         >
-          <div id="hs_embed_signup_scroll" className="main__email-section-form">
-            <input
-              type="email"
-              name="email"
-              className="input-text main__email-section-input"
-              id="email"
-              placeholder="email address"
-              onChange={e => setEmail(e.target.value)}
-              required
-            />
-            <div className="flex-button-container">
-              <input
-                type="submit"
-                value="Subscribe"
-                name="subscribe"
-                id="hs-embedded-subscribe"
-                className="button main__email-section-button"
-              />
-            </div>
-          </div>
-        </form>
+          {({ values, isSubmitting, handleChange, touched, errors }) => (
+            <Form>
+              <div
+                id="embed_signup_scroll"
+                className="main__email-section-form"
+              >
+                <Field
+                  type="email"
+                  name="email"
+                  placeholder="email address"
+                  className={classnames(
+                    'input-text main__email-section-input',
+                    { 'input-error': touched.email && errors.email }
+                  )}
+                />
+                <div className="flex-button-container">
+                  <Button
+                    type="submit"
+                    text="Subscribe"
+                    isDisabled={isSubmitting}
+                    className="button--inverted main__email-section-button"
+                  />
+                </div>
+              </div>
+              <div>
+                {touched.email && errors.email ? (
+                  <Error>{errors.email}</Error>
+                ) : null}
+              </div>
+            </Form>
+          )}
+        </Formik>
       </div>
     </div>
   );
