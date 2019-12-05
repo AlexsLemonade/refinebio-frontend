bc25d63a30e007dd13a36d14405fb4b6f063c644
David Mejia <davidsmejia@gmail.com>
2019-12-05 14:35:50 -0500

    adding api token tracking for knowing sessions for downloads

--- src/state/token.js
+++ src/state/token.js
@@ -1,9 +1,19 @@
+import ReactGA from 'react-ga';
 import { Ajax } from '../common/helpers';
 
 const LOAD_TOKEN = 'LOAD_TOKEN';
 const CLEAR_TOKEN = 'CLEAR_TOKEN';
 
 const initialState = null;
+
+const tokenEvent = event => {
+  if (process.env.NODE_ENV === 'production') {
+    ReactGA.event({
+      category: 'Token',
+      ...event,
+    });
+  }
+};
 
 export default function token(state = initialState, action) {
   const { type, data } = action;
@@ -21,14 +31,15 @@ export const clearToken = () => ({
   type: CLEAR_TOKEN,
 });
 
-export const loadToken = token => ({
+export const loadToken = data => ({
   type: LOAD_TOKEN,
-  data: token,
+  data,
 });
 
 export const createToken = () => async dispatch => {
   const { id: tokenId } = await Ajax.post('/v1/token/');
   await Ajax.put(`/v1/token/${tokenId}/`, { is_activated: true });
   dispatch(loadToken(tokenId));
+  tokenEvent({ action: 'Create API Token', label: tokenId });
   return tokenId;
 };
