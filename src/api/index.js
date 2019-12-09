df589f6d92bb615aa1620e6f507c6fe33d92c32f
Ariel <arielsvn@gmail.com>
2019-12-09 11:40:46 -0500

    add api/index.js

---
+++ src/api/index.js
@@ -0,0 +1,19 @@
+export {
+  getAllDetailedSamples,
+  getGenomeBuild,
+  getDetailedSample,
+} from './samples';
+
+export { getExperiment } from './experiments';
+
+export { fetchCompendiaData, fetchCompendium } from './compendia';
+
+export { fetchDashboardData } from './dashboad';
+
+export {
+  createDataSet,
+  formatExperiments,
+  getDataSet,
+  getDataSetDetails,
+  updateDataSet,
+} from './dataSet';
