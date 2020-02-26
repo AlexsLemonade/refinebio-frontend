97d065b93d2c504ce2bb09f3dfa159bcaf0b15ee
Ariel <arielsvn@gmail.com>
2020-02-26 10:14:54 -0500

    Add bundle analyzer commands

--- next.config.js
+++ next.config.js
@@ -2,18 +2,25 @@ const withCSS = require('@zeit/next-css');
 const withSass = require('@zeit/next-sass');
 const withImages = require('next-images');
 
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+  enabled: process.env.ANALYZE === 'true',
+});
+
 const ApiHost = process.env.REACT_APP_API_HOST || 'https://api.refine.bio';
 
 module.exports = withImages(
-  withSass({
-    env: {
-      REACT_APP_API_HOST: ApiHost,
-    },
-    // webpack: (config, { dev, webpack }) => {
-    //   if (!dev) {
-    //     config.plugins.push(new webpack.optimize.UglifyJsPlugin());
-    //   }
-    //   return config;
-    // },
-  })
+  withSass(
+    withBundleAnalyzer({
+      target: 'serverless',
+      env: {
+        REACT_APP_API_HOST: ApiHost,
+      },
+      // webpack: (config, { dev, webpack }) => {
+      //   if (!dev) {
+      //     config.plugins.push(new webpack.optimize.UglifyJsPlugin());
+      //   }
+      //   return config;
+      // },
+    })
+  )
 );
