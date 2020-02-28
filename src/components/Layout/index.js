f5626340d9d5e425b19d567c2b802fa27c2b87bc
Ariel <arielsvn@gmail.com>
2020-02-28 11:03:09 -0500

    Use router hook in Layout

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
-import { withRouter } from 'next/router';
+import { useRouter } from 'next/router';
 import Header from '../Header';
 import Footer from '../Footer';
 import Notification from '../Notification';
 
-let Layout = ({ children, router: location }) => {
+export default function Layout({ children }) {
   // in some cases it's useful to mark the root object with a class to identify the current page, in case
   // some pages want to make modifications to the layout object
+  const location = useRouter();
   const pageClass =
     location.pathname !== '/'
       ? `page-${location.pathname.substr(1)}`
@@ -20,7 +21,4 @@ let Layout = ({ children, router: location }) => {
       <Footer />
     </div>
   );
-};
-Layout = withRouter(Layout);
-
-export default Layout;
+}
