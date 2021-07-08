c80bbec7d0ce72b35595e743576a8c677681d437
David Mejia <davidsmejia@gmail.com>
2021-07-08 15:58:07 -0400

    remove getRunningJobs

--- src/api/jobs.js
+++ src/api/jobs.js
@@ -11,21 +11,3 @@ export async function getProcessorJobs(accessionCode) {
     sample_accession_code: accessionCode,
   });
 }
-
-export async function getRunningJobs(accessionCode) {
-  const [processorJobs, downloaderJobs] = await Promise.all([
-    Ajax.get(`/v1/jobs/downloader/`, {
-      sample_accession_code: accessionCode,
-      limit: 500,
-    }),
-    Ajax.get(`/v1/jobs/processor/`, {
-      sample_accession_code: accessionCode,
-      limit: 500,
-    }),
-  ]);
-
-  const jobs = [processorJobs, downloaderJobs];
-
-  // filter out non-running jobs
-  return jobs.filter(job => job.is_queued).map(job => job.batch_job_id);
-}
