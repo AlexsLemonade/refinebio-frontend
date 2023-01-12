b7e12d2f98f2bab7f8f19de9a1c7df29c3ebebfe
David Mejia <davidsmejia@gmail.com>
2023-01-12 11:10:26 -0500

    remove old image plugin

--- next.config.js
+++ next.config.js
@@ -1,5 +1,4 @@
 const path = require('path');
-const withImages = require('next-images');
 const withBundleAnalyzer = require('@next/bundle-analyzer')({
   enabled: process.env.ANALYZE === 'true',
 });
